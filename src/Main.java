e5eac18b341e7dfa4c2a8af1956fb8f1c20dd957
yujin-5 <yonggari_gari@naver.com>
2024-11-20 10:41:45 +0900

    세번째 커밋

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ㅋㅋㅋㅋ";
+        System.out.println("ㅋㅋㅋㅋ123");
         }
     }
