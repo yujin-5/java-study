381edefbeba2a9230a9e5a152c83aa01075eb64b
yujin-5 <yonggari_gari@naver.com>
2024-12-18 17:48:08 +0900

    혼공자 예시 학습 (12.18)

---
+++ src/practice/hongongja/sec06/exam06/CarExample.java
@@ -0,0 +1,18 @@
+package practice.hongongja.sec06.exam06;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.setSpeed(-50);
+        System.out.println("현재 속도: " + myCar.getSpeed());
+
+        myCar.setSpeed(60);
+        if(!myCar.isStop()){
+            myCar.setStop(true);
+        }
+
+        System.out.println("현재 속도: " + myCar.getSpeed());
+
+    }
+}
